8101e6e17261c11e30bfd8ebc474fe621fc8963f
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-11-17 23:00:02 -0500

    Adds component architecture fromm clothing shop
    Imports all components in order to build barebones React with Redux
    template.

---
+++ components/test.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+import './hidavehyphen.styles.scss'
+
+class HiDaveHyphen extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+        key: 'value'
+    }
+
+  }
+
+
+  render() {
+    return (
+
+      <div> </div>
+
+    )
+
+  }
+
+}
+
+export default HiDaveHyphen
--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom'
+
 import './App.css';
+
+import HomePage from './pages/homepage/home.component'
+import Header from './components/header/header.component'
+
 
 class App extends Component {
 
@@ -16,21 +21,13 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. Hello {this.state.name}!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header className='header' />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+          </Switch>
+        
+          
       </div>
     );
 
