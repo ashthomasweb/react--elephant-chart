efa0089ebf9d5e4b82a379d12c64568025077ee9
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-11-30 14:47:50 -0500

    Adds offset to state
    Implements continuous update of state to immediately drag note, and
    finds offset so that drag and drop item remains in proper relationship
    as the mouse when it was clicked.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -18,12 +18,16 @@ class Directory extends Component {
           top: '',
           left: '',
           imageUrl: logo,
+          mouseOffsetX: 0,
+          mouseOffsetY: 0
         },
         {
           id: 2,
           top: '',
           left: '350px',
           imageUrl: logo,
+          mouseOffsetX: 0,
+          mouseOffsetY: 0
         },
       ]
     };
@@ -32,58 +36,16 @@ class Directory extends Component {
   }
 
   tester = (input) => {
-    // let temp = input.left
-    // console.log(`Hi Dave, I'm ${input.left} ${input.top}`)
     this.setState( { currentDrag: input })
-    
-    // const updateNote = () => this.state.notes.find((note) => {
-    //   console.log(this.state.notes.indexOf(note.id))
-    //   return note.id === input.id
-    // })
-
-    // // this.setState( { })
-    // console.log('update:', updateNote())
-
+    var newNote = { ...this.state.currentDrag }
+    var newIndex = newNote.id - 1
+    var notes = [...this.state.notes]
+    notes[newIndex] = newNote
+    this.setState({ notes })
   }
   
   dropHandler = (e) => {
-    // console.log(this.state)
-    // console.log(this.state.currentDrag)
-
-
-    var newNote = { ...this.state.currentDrag }
-    var newIndex = newNote.id - 1
-    console.log(newIndex)
-
-    var notes = [...this.state.notes]
-
-    notes[newIndex] = newNote
-    this.setState({ notes }, () => {
-      this.render()
-      console.log('within setState:', notes) 
-    })
-    console.log('after setState:', this.state.notes)
-
-
-    // this.state.notes.forEach( (note) => {
-    //   if (this.state.currentDrag.id === note.id) {
-    //     this.setState( { notes[currentDrag.id].left: currentDrag.left })
-    //   }
-    // })
-
-
-    // var note = { ...this.state.currentDrag}
-    // var index = note.id - 1
-    // console.log(note)
-    // console.log(this.state.notes[1])
-    // console.log(index)
-    // this.setState({ this.state.notes[index]: note })
-    
-
-  
-
-    // identify the correct note and update info
-    // get coords from state
+    this.tester()
     // database storage for board persistence
   }
 
@@ -98,12 +60,12 @@ class Directory extends Component {
               <NoteItem key={id} dave={this.tester} value={id} { ...sectionProps}/>
           )) }    
       <CustomButton 
-      style={{ top: '550px', position: 'absolute' }}
+      style={{ bottom: '50px', position: 'absolute' }}
       onClick={() => console.log(this.state)}>
       Log Notes State
     </CustomButton>
     <CustomButton 
-      style={{ top: '550px', left: '300px', position: 'absolute' }}
+      style={{ bottom: '50px', left: '300px', position: 'absolute' }}
       onClick={() => console.log(this.state.currentDrag)}>
       Log CurrentDrag State
     </CustomButton>
--- src/components/note-item/note-item.component.jsx
+++ src/components/note-item/note-item.component.jsx
@@ -13,32 +13,34 @@ class NoteItem extends Component {
       top: '',
       left: '',
       imageUrl: logo,
+      mouseOffsetX: 0,
+      mouseOffsetY: 0
     }
 
   }
   
+  mouseOffset = (e) => {
+    const mouseOffsetY = e.pageY - parseFloat(getComputedStyle(e.target).top)
+    const mouseOffsetX = e.pageX - parseFloat(getComputedStyle(e.target).left)
+    this.setState({mouseOffsetY, mouseOffsetX}, () => this.props.dave(this.state))
+  }
+  
   // sets current position of dragged note to Note Component's state, makes it immediately accesible with anon callback.
   dragHandler = (e) => {
-    this.setState( { left: e.clientX + 'px', top: e.clientY + 'px', id: this.props.value }, () => this.props.dave(this.state) )
+    this.setState( { left: e.clientX - this.state.mouseOffsetX + 'px', top: e.clientY - this.state.mouseOffsetY + 'px', id: this.props.value }, () => this.props.dave(this.state) )
   }
   
   render() {
     const { value, imageUrl, size, left, top/*title,  history, linkUrl, match*/ } = this.props
 
     return (
-        <div style={{ left: `${left}`, top: `${top}` }}
-        // onClick={() => history.push(`${match.url}${linkUrl}`)}
+        <div style={{ left: `${left}`, top: `${top}`, backgroundImage: `url(${imageUrl})` }}
           className={`${size} menu-item`}
+          onMouseDown={this.mouseOffset}
           onDrag={this.dragHandler}
           id={value}
           draggable
           >
-          <div
-            className='background-image'
-            style={{
-              backgroundImage: `url(${imageUrl})`
-            }}
-          />
           {/* <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP</span>
